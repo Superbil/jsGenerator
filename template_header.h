7fa7fb47fc7ecd0229aac1d6ba5e73b1e10e90cd
Superbil <superbil@gmail.com>
2014-05-01 05:53:55 +0800

    add template_files

---
+++ template_header.h
@@ -0,0 +1,23 @@
+//
+//  {ObjC_ClassName}.h
+//
+//  Created by Superbil on 2014/4/22.
+//
+//
+
+#import "JSONModel.h"
+
+@protocol {ObjC_ClassName}
+@end
+
+/**
+ {JSONSchemaTitle}
+
+ {JSONSchemaDescription}
+
+ JSON Schema: {JSONSchemaURL}
+ */
+
+@interface {ObjC_ClassName} : JSONModel
+{ObjC_Properties}
+@end
---
+++ template_implementation.m
@@ -0,0 +1,12 @@
+//
+//  {ClassName}.m
+//
+//  Created by Superbil on 2014/4/22.
+//
+//
+
+#import "{ObjC_ClassName}.h"
+
+@implementation {ObjC_ClassName}
+{ObjC_Implementation}
+@end
